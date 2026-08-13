6b6d3ecfa56b7f01f8e6a81fd41a08e918dbae83
AbrarAlkathami <AbrarAlkathami@gmail.com>
2026-08-13 21:10:34 +0300

    chore: cass cards with grids

---
+++ case-interview-ai-coash-frontend/src/features/cases/components/caseCard/CaseCard.tsx
@@ -0,0 +1,35 @@
+import Badge from "../../../../components/common/Badge/Badge";
+import Button from "../../../../components/common/Button/Button";
+import style from "../caseCard/CaseCard.module.css";
+
+interface CaseCardProps {
+  id: number;
+  caseName: string;
+  caseType: string;
+  companyName: string;
+  difficulty: string;
+}
+
+function CaseCard({
+  caseName,
+  caseType,
+  companyName,
+  difficulty,
+}: CaseCardProps) {
+  const caseDetails = [caseType, difficulty, companyName];
+  return (
+    <div className={style.caseCard}>
+      <h5 className={style.caseCardTitle}>{caseName}</h5>
+      <div className={style.caseCardBadges}>
+        {caseDetails.map((item) => (
+          <Badge key={item} text={item} />
+        ))}
+      </div>
+      <div className={style.caseCardActions}>
+        <Button>{companyName}</Button>
+      </div>
+    </div>
+  );
+}
+
+export default CaseCard;
